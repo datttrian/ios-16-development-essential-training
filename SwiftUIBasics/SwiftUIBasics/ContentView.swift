b7ea9b83be890d7690b42be2caa88b328fa9c5d6
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-09 17:12:08 -0500

    Modifying fonts and spacing in SwiftUI

--- SwiftUIBasics/SwiftUIBasics/ContentView.swift
+++ SwiftUIBasics/SwiftUIBasics/ContentView.swift
@@ -11,12 +11,16 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Weather")
+                .font(.largeTitle)
+                .fontWeight(.thin)
+                .foregroundColor(Color(hue: 1.0, saturation: 0.0, brightness: 0.388))
             HStack {
-                Image(systemName: "arrow.right.arrow.left.square")
+                Image(systemName: "sun.max.fill")
+                    .foregroundColor(Color.yellow)
                     .imageScale(.large)
                     .foregroundStyle(.tint)
-                Text("Hello, people!")
-                    .font(.largeTitle)
+                Text("Sunday")
+                
             }
             
         }
