83ba5ee6140f53eb7d7231e8b6163bab839fb5cd
ttran375 <ttran375@my.centennialcollege.ca>
2024-01-09 20:06:51 +0000

    Drag and drop user interface creation with storyboards

---
+++ Storyboards/Storyboards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Storyboards
+//
+//  Created by datttrian on 2024-01-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
