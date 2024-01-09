a7b8620f65936051df8a09d565f6bfca1ff9012e
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-09 16:37:50 -0500

    Solution: Storyboards

--- Storyboards/Storyboards/ViewController.swift
+++ Storyboards/Storyboards/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         textField.becomeFirstResponder()
+        label.text = ""
         
     }
     
